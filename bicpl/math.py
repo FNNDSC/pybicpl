c6211805953f2a57be6a3630cd852d3f87a662e0
Jennings Zhang <jenni_zh@protonmail.com>
2022-03-10 20:03:49 -0500

    Fix difference_average types

--- bicpl/math.py
+++ bicpl/math.py
@@ -9,17 +9,17 @@ import numpy as np
 import numpy.typing as npt
 
 
-def _local_da(neighbors, data, point_index) -> npt.NDArray[np.float]:
+def _local_da(neighbors, data, point_index) -> np.float64:
     """
     Average absolute difference between the
     value at point_index and its neighbors.
     """
-    return np.mean(np.abs(data[point_index] - data[tuple(neighbors)]))
+    return np.mean(np.abs(data[point_index] - data[list(neighbors)]))  # type: ignore
 
 
 def difference_average(neighbor_graph: tuple[set[int], ...],
-                       data: npt.NDArray[np.float]
-                       ) -> Iterator[npt.NDArray[np.float]]:
+                       data: npt.NDArray[np.floating]
+                       ) -> Iterator[np.float64]:
     """
     Average absolute difference between the
     value at point_index and its neighbors
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if not os.getenv('GITHUB_WORKFLOW') == 'Publish' \
 
 setup(
     name='pybicpl',
-    version='0.3.0',
+    version='0.3.1',
     packages=['bicpl'],
     url='https://github.com/FNNDSC/pybicpl',
     license='MIT',
